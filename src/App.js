149c483d7736498538da7d6868373b9b7bf0ffd2
Amdrii <andreyk557@gmail.com>
2023-03-22 12:42:11 +0200

    feat[jsx-from-str]

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <input />
+      <label>Bill total</label>
+      <input type="text" placeholder={"0.00"} />
+      <label>Tip</label>
+      <input type="text" placeholder={"0.00"} />
+      <div className="summary">
+        <div className="split">
+          <label>Split</label>
+          <div className="split-control">
+            <button>-</button>
+            <span>1</span>
+            <button>+</button>
+          </div>
+        </div>
+        <div className="result">
+          <label>Total</label>
+          <span>$0.00</span>
+        </div>
+      </div>
     </div>
   );
 }

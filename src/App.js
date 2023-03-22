e0b113c84a1cbdcae94aae509ccb1d0dda468947
Amdrii <andreyk557@gmail.com>
2023-03-22 13:22:21 +0200

    feat[tip/bill/state]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [bill, setBill] = useState("");
+  const [tip, setTip] = useState("10%");
+
+  function handleTipChange(e) {
+    let value = e.target.value.replace("%", "");
+    if (value.indexOf("%") === -1) {
+      value = value + "%";
+    }
+    setTip(value);
+  }
+
   return (
     <div className="App">
       <label>Bill total</label>
-      <input type="text" placeholder={"0.00"} />
+      <input
+        type="text"
+        placeholder={"0.00"}
+        value={bill}
+        onChange={(e) => setBill(e.target.value)}
+      />
       <label>Tip</label>
-      <input type="text" placeholder={"0.00"} />
+      <input
+        type="text"
+        placeholder={"0.00"}
+        value={tip}
+        onChange={handleTipChange}
+      />
       <div className="summary">
         <div className="split">
           <label>Split</label>
